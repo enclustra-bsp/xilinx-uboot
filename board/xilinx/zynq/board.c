f4c416aaf0663f79f50124833c7a85a4a32a7553
Tomasz Gorochowik <tgorochowik@antmicro.com>
2015-07-07 12:33:42 +0200

    zynq: use the same eth PHY timings for every ZX
    Clock skew settings moved out of ZX2 define into a more general
    ZX defines to use the same code across all Enclustra ZX targets.

--- board/xilinx/zynq/board.c
+++ board/xilinx/zynq/board.c
@@ -140,39 +140,6 @@ int zx_set_storage_cmd(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]
 {
 	return CMD_RET_SUCCESS;
 }
-
-/* Setup clock skews for ethernet PHY */
-
-#define MARS_ZX2_CLK_SKEW_VAL	0x3FF
-#define MARS_ZX2_DATA_SKEW_VAL	0x00
-
-int board_phy_config(struct phy_device *phydev)
-{
-	/* min rx/tx ctrl delay */
-	ksz9031_phy_extended_write(phydev, 2,
-				   MII_KSZ9031_EXT_RGMII_CTRL_SIG_SKEW,
-				   MII_KSZ9031_MOD_DATA_NO_POST_INC,
-				   MARS_ZX2_DATA_SKEW_VAL);
-	/* min rx delay */
-	ksz9031_phy_extended_write(phydev, 2,
-				   MII_KSZ9031_EXT_RGMII_RX_DATA_SKEW,
-				   MII_KSZ9031_MOD_DATA_NO_POST_INC,
-				   MARS_ZX2_DATA_SKEW_VAL);
-	/* max tx delay */
-	ksz9031_phy_extended_write(phydev, 2,
-				   MII_KSZ9031_EXT_RGMII_TX_DATA_SKEW,
-				   MII_KSZ9031_MOD_DATA_NO_POST_INC,
-				   MARS_ZX2_DATA_SKEW_VAL);
-	/* rx/tx clk skew */
-	ksz9031_phy_extended_write(phydev, 2,
-				   MII_KSZ9031_EXT_RGMII_CLOCK_SKEW,
-				   MII_KSZ9031_MOD_DATA_NO_POST_INC,
-				   MARS_ZX2_CLK_SKEW_VAL);
-
-	phydev->drv->config(phydev);
-
-	return 0;
-}
 #endif
 
 #if defined(CONFIG_MARS_ZX) || defined(CONFIG_MARS_ZX2) || defined(CONFIG_MERCURY_ZX)
@@ -298,6 +265,40 @@ int board_eth_init(bd_t *bis)
 #endif
 	return ret;
 }
+
+/* Setup clock skews for ethernet PHY on Enclustra ZX boards */
+#if defined(CONFIG_MARS_ZX) || defined(CONFIG_MARS_ZX2) || defined(CONFIG_MERCURY_ZX)
+#define ENCLUSTRA_ZX_CLK_SKEW_VAL	0x3FF
+#define ENCLUSTRA_ZX_DATA_SKEW_VAL	0x00
+
+int board_phy_config(struct phy_device *phydev)
+{
+	/* min rx/tx ctrl delay */
+	ksz9031_phy_extended_write(phydev, 2,
+				   MII_KSZ9031_EXT_RGMII_CTRL_SIG_SKEW,
+				   MII_KSZ9031_MOD_DATA_NO_POST_INC,
+				   ENCLUSTRA_ZX_DATA_SKEW_VAL);
+	/* min rx delay */
+	ksz9031_phy_extended_write(phydev, 2,
+				   MII_KSZ9031_EXT_RGMII_RX_DATA_SKEW,
+				   MII_KSZ9031_MOD_DATA_NO_POST_INC,
+				   ENCLUSTRA_ZX_DATA_SKEW_VAL);
+	/* max tx delay */
+	ksz9031_phy_extended_write(phydev, 2,
+				   MII_KSZ9031_EXT_RGMII_TX_DATA_SKEW,
+				   MII_KSZ9031_MOD_DATA_NO_POST_INC,
+				   ENCLUSTRA_ZX_DATA_SKEW_VAL);
+	/* rx/tx clk skew */
+	ksz9031_phy_extended_write(phydev, 2,
+				   MII_KSZ9031_EXT_RGMII_CLOCK_SKEW,
+				   MII_KSZ9031_MOD_DATA_NO_POST_INC,
+				   ENCLUSTRA_ZX_CLK_SKEW_VAL);
+
+	phydev->drv->config(phydev);
+
+	return 0;
+}
+#endif
 
 #ifdef CONFIG_CMD_MMC
 int board_mmc_init(bd_t *bd)
