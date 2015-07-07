ea8d3250d8d2cfa56dd83fb179dba52dc5890d20
Tomasz Gorochowik <tgorochowik@antmicro.com>
2015-07-07 14:05:48 +0200

    zynq: add MarsZX3 on EB1 config/devicetree
    MarsZX3 setup on EB1 baseboard differs from setups on other baseboards
    therefore a new set of configuration files is required.

---
+++ include/configs/zynq_mars_zx3_eb1.h
@@ -0,0 +1,45 @@
+/*
+ * (C) Copyright 2013 Xilinx, Inc.
+ * (C) Copyright 2015 Antmicro Ltd
+ *
+ * Configuration for Enclustra Mars ZX3 SOM on Enclustra MARS EB1 Baseboard
+ *
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_MARS_ZX3_EB1_H
+#define __CONFIG_ZYNQ_MARS_ZX3_EB1_H
+
+#define CONFIG_MARS_ZX
+
+#define CONFIG_CPU_FREQ_HZ		666666666
+
+#define CONFIG_SYS_SDRAM_SIZE		(512 * 1024 * 1024)
+
+#define CONFIG_ZYNQ_SERIAL_UART0
+#define CONFIG_ZYNQ_GEM0
+#define CONFIG_ZYNQ_GEM_PHY_ADDR0	0
+
+#define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_ZYNQ_USB
+#define CONFIG_ZYNQ_SDHCI0
+#define CONFIG_ZYNQ_QSPI
+#define CONFIG_NAND_ZYNQ
+
+#define CONFIG_ZYNQ_BOOT_FREEBSD
+
+/* Select Micrel PHY */
+#define CONFIG_PHY_MICREL
+
+#include <configs/zynq-common.h>
+#include <configs/enclustra_zx_common.h>
+
+/* Unselect Marvell PHY (selected by zynq-common) */
+#ifdef CONFIG_PHY_MARVELL
+#undef CONFIG_PHY_MARVELL
+#endif
+
+#endif /* __CONFIG_ZYNQ_MARS_ZX3_EB1_H */
