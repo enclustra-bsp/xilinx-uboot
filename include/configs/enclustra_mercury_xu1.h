12ecbd234c4921018936ed6566377956950a9b6a
Karol Gugala <kgugala@antmicro.com>
2016-12-01 16:11:05 +0100

    config: enclustra_mercury_zx1: use enclustra common settings

--- include/configs/enclustra_mercury_xu1.h
+++ include/configs/enclustra_mercury_xu1.h
@@ -1,45 +1,19 @@
 /*
- * Configuration for Xilinx ZynqMP zcu102
+ * Configuration for Enclustra Mercury XU1 SOM
  *
  * (C) Copyright 2015 Xilinx, Inc.
- * Michal Simek <michal.simek@xilinx.com>
+ * (C) Copyright 2016 Antmicro Ltd
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#ifndef __CONFIG_ZYNQMP_ZCU102_H
-#define __CONFIG_ZYNQMP_ZCU102_H
+#ifndef __CONFIG_ENCLUSTRA_XU1_H
+#define __CONFIG_ECNLUSTRA_XU1_H
 
 #define CONFIG_ZYNQ_SDHCI1
 #define CONFIG_ZYNQ_I2C0
 #define CONFIG_ZYNQ_I2C1
-/*#define CONFIG_SYS_I2C_MAX_HOPS		1
-#define CONFIG_SYS_NUM_I2C_BUSES	18
-#define CONFIG_SYS_I2C_BUSES	{ \
-				{0, {I2C_NULL_HOP} }, \
-				{0, {{I2C_MUX_PCA9544, 0x75, 0} } }, \
-				{0, {{I2C_MUX_PCA9544, 0x75, 1} } }, \
-				{0, {{I2C_MUX_PCA9544, 0x75, 2} } }, \
-				{1, {I2C_NULL_HOP} }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 0} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 1} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 2} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 3} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 4} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 0} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 1} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 2} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 3} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 4} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 5} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 6} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 7} } }, \
-				}
-*/
 #define CONFIG_SYS_I2C_ZYNQ
-#define CONFIG_PCA953X
-#define CONFIG_CMD_PCA953X
-#define CONFIG_CMD_PCA953X_INFO
 
 #define CONFIG_SATA_CEVA
 
@@ -47,11 +21,9 @@
 
 #define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	1
 #define CONFIG_CMD_EEPROM
-#define CONFIG_ZYNQ_EEPROM_BUS		5
-#define CONFIG_ZYNQ_GEM_EEPROM_ADDR	0x54
-#define CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET	0x20
 
 #include <configs/xilinx_zynqmp.h>
+#include <configs/enclustra_zx_common.h>
 
 /*#ifdef CONFIG_ENV_IS_NOWHERE
 #undef CONFIG_ENV_IS_NOWHERE
@@ -59,4 +31,4 @@
 
 #define CONFIG_ENV_IS_IN_SPI_FLASH*/
 
-#endif /* __CONFIG_ZYNQMP_ZCU102_H */
+#endif /* __CONFIG_ENCLUSTRA_XU1_H */
