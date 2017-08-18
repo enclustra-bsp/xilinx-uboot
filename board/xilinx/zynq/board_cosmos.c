e9a413c430fb910fdcde394fd6a353bfc6fa52e6
Maciej Mikunda <mmikunda@antmicro.com>
2017-08-18 12:27:36 +0200

    board: cosmos: set proper mux for SDIO1
    On Cosmos XZQ10 SDIO1 interface is routed through the MIO10-MIO15 pins.

--- board/xilinx/zynq/board_cosmos.c
+++ board/xilinx/zynq/board_cosmos.c
@@ -4,7 +4,19 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/io.h>
 
+#define SDIO1MUX 0x280
+
 int last_stage_init(void)
 {
+    /* Set SDIO1 pinctrl */
+    zynq_slcr_unlock();
+    writel(SDIO1MUX,  &slcr_base->mio_pin[10]); /* Pin 0, SDIO1 */
+    writel(SDIO1MUX,  &slcr_base->mio_pin[11]); /* Pin 1, SDIO1 */
+    writel(SDIO1MUX,  &slcr_base->mio_pin[12]); /* Pin 2, SDIO1 */
+    writel(SDIO1MUX,  &slcr_base->mio_pin[13]); /* Pin 3, SDIO1 */
+    writel(SDIO1MUX,  &slcr_base->mio_pin[14]); /* Pin 4, SDIO1 */
+    writel(SDIO1MUX,  &slcr_base->mio_pin[15]); /* Pin 5, SDIO1 */
+    zynq_slcr_lock();
+
     return 0;
 }
