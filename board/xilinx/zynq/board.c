7b5fd9e34f1d6fe926cb013b9280b29e06f76d6b
Tomasz Michalak <tmichalak@antmicro.com>
2018-05-29 11:10:36 +0200

    board: zynq: add workaround for incorrect MAC programmed to EEPROM

--- board/xilinx/zynq/board.c
+++ board/xilinx/zynq/board.c
@@ -328,6 +328,17 @@ int board_late_init(void)
 			if(eeproms[i].mac_reader(eeproms[i].i2c_addr, hwaddr))
 				continue;
 
+			/* Workaround for incorrect MAC address caused by
+			 * flashing to EEPROM adresses like 20:B0:F0:XX:XX:XX
+			 * instead of 20:B0:F7:XX:XX:XX */
+			hwaddr[2] = (hwaddr[2] == 0xF0) ? 0xF7 : hwaddr[2];
+
+			/* Check if the value is a valid mac registered for
+			 * Enclustra  GmbH */
+			hwaddr_h = hwaddr[0] | hwaddr[1] << 8 | hwaddr[2] << 16;
+			if ((hwaddr_h & 0xFFFFFF) != ENCLUSTRA_MAC)
+				continue;
+
 			/* Format the address using a string */
 			sprintf(hwaddr_str,
 				"%02X:%02X:%02X:%02X:%02X:%02X",
@@ -337,12 +348,6 @@ int board_late_init(void)
 				hwaddr[3],
 				hwaddr[4],
 				hwaddr[5]);
-
-			/* Check if the value is a valid mac registered for
-			 * Enclustra  GmbH */
-			hwaddr_h = hwaddr[0] | hwaddr[1] << 8 | hwaddr[2] << 16;
-			if ((hwaddr_h & 0xFFFFFF) != ENCLUSTRA_MAC)
-				continue;
 
 			/* Set the actual env variable */
 			setenv("ethaddr", hwaddr_str);
