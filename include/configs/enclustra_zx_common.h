0da8ff9bd674b118af963d3c8fba70c45649a6e8
Joanna Brozek <jbrozek@antmicro.com>
2019-03-15 13:54:31 +0100

    include: configs: change the devictree load address
    The devictree is loaded after Image,
    which size was extended after the Linux update.

--- include/configs/enclustra_zx_common.h
+++ include/configs/enclustra_zx_common.h
@@ -71,7 +71,7 @@
 /* Kernel image name */
 #ifdef CONFIG_ARCH_ZYNQMP
 #define KERNEL_IMAGE_FILE "Image"
-#define DTB_LOADADDR "0x7100000"
+#define DTB_LOADADDR "0x7180000"
 #define QSPI_STRING "/amba/spi@ff0f0000/flash@0/partition"
 #else
 #define KERNEL_IMAGE_FILE "uImage"
