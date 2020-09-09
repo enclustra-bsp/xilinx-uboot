37b90ab2a8b55261e06c1ac1aae96ebee87f1ff4
tholzsche <tillholzscheiter@enclustra.com>
2020-09-09 11:52:20 +0200

    drivers: mmc: sdhci.c cleanup

--- drivers/mmc/sdhci.c
+++ drivers/mmc/sdhci.c
@@ -276,7 +276,6 @@ static int sdhci_send_command(struct mmc *mmc, struct mmc_cmd *cmd,
 			return -ETIMEDOUT;
 	}
 
-	sdhci_writel(host, SDHCI_INT_ALL_MASK, SDHCI_INT_STATUS);
 	mask = SDHCI_CMD_INHIBIT | SDHCI_DATA_INHIBIT;
 
 	/* We shouldn't wait for data inihibit for stop commands, even
@@ -838,7 +837,7 @@ int sdhci_setup_cfg(struct mmc_config *cfg, struct sdhci_host *host,
 	if (host->quirks & SDHCI_QUIRK_BROKEN_VOLTAGE)
 		cfg->voltages |= host->voltages;
 
-	cfg->host_caps = MMC_MODE_HS | MMC_MODE_HS_52MHz | MMC_MODE_8BIT | MMC_MODE_4BIT;
+	cfg->host_caps |= MMC_MODE_HS | MMC_MODE_HS_52MHz | MMC_MODE_8BIT | MMC_MODE_4BIT;
 
 	/* Since Host Controller Version3.0 */
 	if (SDHCI_GET_VERSION(host) >= SDHCI_SPEC_300) {
